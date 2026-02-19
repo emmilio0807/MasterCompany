14bc2e8b7e1d549c86174d08be780ee7871e5115
emilio0807 <forplaylol@outlook.es>
2026-02-18 17:06:38 -0800

    CORS config

--- MasterCompany.API/Program.cs
+++ MasterCompany.API/Program.cs
@@ -20,26 +20,16 @@ builder.Services.AddSwaggerGen();
 // Inyecci�n de dependencias
 builder.Services.AddScoped<IEmployeeData, EmployeeData>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAngular", policy =>
-    {
-        policy.WithOrigins("http://localhost:4200")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
-});
 
 var app = builder.Build();
-app.UseCors("AllowAngular");
-
-app.UseCors("AllowAngular");
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowAngular");
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
